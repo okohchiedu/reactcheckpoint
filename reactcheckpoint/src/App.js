c151e28aa37b4843a1f56e8bef81a2a63165ab63
okohchiedu <okohthomas35@gmail.com>
2023-04-08 12:54:50 +0100

    first commit

---
+++ reactcheckpoint/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import BasicExample from './component/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>sign up!</h1>
+      <div className='innerApp'>
+      <BasicExample/>
+      </div>
+      
+      
+    </div>
+  );
+}
+
+export default App;
